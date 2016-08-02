7538b65994660d07e904bdc569cd963ef7b01307
ipavlenko <ipavlenko@inbox.ru>
2016-08-02 21:06:37 +0500

    Structure update

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('scripts', function() {
         // .pipe(browserify())
         .pipe(concat('widgets.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('dist/js/'))
 })
 
 gulp.task('styles', function() {
@@ -19,7 +19,7 @@ gulp.task('styles', function() {
         .pipe(cleancss())
         .pipe(concat('widgets.css'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/css/'));
 })
 
 gulp.task('templates', function() {
@@ -27,7 +27,7 @@ gulp.task('templates', function() {
         .pipe(sourcemaps.init())
         .pipe(concat('widgets.htm'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/htm/'));
 })
 
 gulp.task('default', [
--- src/widgets/composites/palette.js
+++ src/widgets/composites/palette.js
@@ -1,5 +1,6 @@
 (function($, Vue, Core, Widgets) {
 
+    Widgets.HeadersGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-headers', 'Headers');
     Widgets.NavigationGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-navigation', 'Navigation');
     Widgets.GalleryGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-gallery', 'Galleries');
 
