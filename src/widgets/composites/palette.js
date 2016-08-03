6a200e2c9b30e8f44a33459a4ad17bc8721123fd
ipavlenko <ipavlenko@inbox.ru>
2016-08-03 18:30:49 +0500

    Structure update

--- src/widgets/composites/palette.js
+++ src/widgets/composites/palette.js
@@ -1,6 +1,7 @@
 (function($, Vue, Core, Widgets) {
 
     Widgets.HeadersGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-headers', 'Headers');
+    Widgets.FootersGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-footers', 'Footers');
     Widgets.NavigationGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-navigation', 'Navigation');
     Widgets.GalleryGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-gallery', 'Galleries');
 
