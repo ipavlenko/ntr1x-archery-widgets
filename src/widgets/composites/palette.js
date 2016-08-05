062e2b5a2086b50ce9a7a0c0c0b998325ba6d294
ipavlenko <ipavlenko@inbox.ru>
2016-08-05 19:28:27 +0500

    Structure update

--- src/widgets/composites/palette.js
+++ src/widgets/composites/palette.js
@@ -2,6 +2,7 @@
 
     Widgets.HeadersGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-headers', 'Headers');
     Widgets.FootersGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-footers', 'Footers');
+    Widgets.OffersGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-offers', 'Offers');
     Widgets.NavigationGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-navigation', 'Navigation');
     Widgets.GalleryGroup = Widgets.Group(Widgets.CompositeCategory, 'default-composite-gallery', 'Galleries');
 
